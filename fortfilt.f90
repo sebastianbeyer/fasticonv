f6c5d3972bff8b0deef5e673209d31484b52b741
Sebastian Beyer <sebastian.beyer@awi.de>
2015-09-16 11:41:16 +0200

    initial naive gauss implemented

--- fortfilt.f90
+++ fortfilt.f90
@@ -2,7 +2,49 @@ module fortfilt
   implicit none
 
 contains
-  subroutine gauss_filter ()
-  end subroutine gauss_filter
+  subroutine naive_gauss (source, filtered, size)
+    implicit none
+    integer, intent(in)                          :: size
+    double precision, intent(in)                 :: source(:,:)
+    double precision, intent(out)                :: filtered(:,:)
+
+    double precision, parameter                  :: PI = 4.*atan(1.)
+
+    integer                                      :: i, j, k, l
+    integer                                      :: ii, jj      ! inside the array
+    integer                                      :: nx, ny
+    integer                                      :: dim(2)
+
+    double precision                             :: val, wsum
+    double precision                             :: dsq         ! distance squared
+    double precision                             :: wght        ! weight
+
+    dim = shape(source)
+    nx = dim(1)
+    ny = dim(2)
+
+    print *, nx, ny
+    do i = 1, nx
+       do j = 1, ny
+          val = 0
+          wsum = 0
+          do k = i-size, i+size     ! inner loop over kernel
+             do l = j-size, j+size  ! inner loop over kernel
+                ii = min(nx, max(1,k))   ! make sure i is always inside the grid (this implies values are extendet (stretched at the boundaries))
+                jj = min(ny, max(1,l))   ! make sure j is always inside the grid (this implies values are extendet (stretched at the boundaries))
+                dsq = (j-l)**2 + (i-k)**2
+                wght = exp(-dsq / (2*size**2)) / (2*PI*size**2)
+                val = val + source(ii,jj) * wght
+                wsum = wsum + wght
+                ! print *, i,j, k, l, ii, jj, dsq
+             end do
+          end do
+          filtered(i,j) = val / wsum
+       end do
+    end do
+    
+
+    
+  end subroutine naive_gauss
 
 end module fortfilt
--- test.f90
+++ test.f90
@@ -1,7 +1,28 @@
 program test_filter
   use fortfilt
   implicit none
+  
+  double precision, dimension(12,8)     :: data
+  double precision, dimension(12,8)     :: filtered_data
+
+  integer                               :: i,j
 
   print *, 'filter test'
+
+  ! fill data with checkerboard pattern
+  do i = 1, 12
+     do j = 1, 8
+        data(i,j) = mod(i+j,2)
+     end do
+  end do
+
+  data = 0
+  data(6,4) = 15
+
+  write(*,"(12f4.1)") data
+
+  call naive_gauss(data, filtered_data, 2)
+  print *, "filtered:"
+  write(*,"(12f4.1)") filtered_data
 
 end program test_filter
